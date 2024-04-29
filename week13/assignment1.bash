888a9ebd842d169e7ce352278a515ae8283906cb
RileyBashaw <112733012+RileyBashaw@users.noreply.github.com>
2024-04-29 00:09:31 -0400

    Update assignment1.bash

--- week13/assignment1.bash
+++ week13/assignment1.bash
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 clear
 
 # filling courses.txt
@@ -8,63 +8,67 @@ courseFile="courses.txt"
 
 function displayCoursesofInst(){
 
-echo -n "Please Input an Instructor Full Name: "
-read instName
+    echo -n "Please Input an Instructor Full Name: "
+    read instName
 
-echo ""
-echo "Courses of $instName :"
-cat "$courseFile" | grep "$instName" | cut -d';' -f1,2 | \
-sed 's/;/ | /g'
-echo ""
+    echo ""
+    echo "Courses of $instName :"
+    grep "$instName" "$courseFile" | cut -d';' -f1,2 | sed 's/;/ | /g'
+    echo ""
 
 }
 
 function courseCountofInsts(){
 
-echo ""
-echo "Course-Instructor Distribution"
-cat "$courseFile" | cut -d';' -f7 | \
-grep -v "/" | grep -v "\.\.\." | \
-sort -n | uniq -c | sort -n -r 
-echo ""
+    echo ""
+    echo "Course-Instructor Distribution"
+    cut -d':' -f7 "$courseFile" | grep -v "/" | grep -v "\.\.\." | sort | uniq -c | sort -nr
+    echo ""
 
 }
 
-# TODO - 1
-# Make a function that displays all the courses in given location
-# function dislaplays course code, course name, course days, time, instructor
-# Add function to the menu
-# Example input: JOYC 310
-# Example output: See the screenshots in canvas
+function classlocations(){
+    echo ""
+    echo -n "Insert Classroom Name: "
+    read Input
+    grep "$Input" "$courseFile" | cut -d';' -f1,2,5,6,7 | sed 's/j/ | /g'
+    echo ""
+}
 
-# TODO - 2
-# Make a function that displays all the courses that has availability
-# (seat number will be more than 0) for the given course code
-# Add function to the menu
-# Example input: SEC
-# Example output: See the screenshots in canvas
+function openseats(){
+    echo ""
+    echo "Open seats within courses"
+    echo -n "Insert Subject Title: "
+    read subjectInput
+    while read -r text
+    do
+        seats=$(echo "$text" | cut -d';' -f4)
+        if [[ "$seats" -gt "0" ]]; then
+            echo "$text" | sed 's/;/ | /g'
+        fi
+    done < "$courseFile"
+    echo ""
+}
 
 while :
 do
-	echo ""
-	echo "Please select and option:"
-	echo "[1] Display courses of an instructor"
-	echo "[2] Display course count of instructors"
-	echo "[5] Exit"
+    echo ""
+    echo "Please select an option:"
+    echo "[1] Display courses of an instructor"
+    echo "[2] Display course count of instructors"
+    echo "[3] Display class locations"
+    echo "[4] Display open seats in a class"
+    echo "[5] Exit"
 
-	read userInput
-	echo ""
+    read userInput
+    echo ""
 
-	if [[ "$userInput" == "5" ]]; then
-		echo "Goodbye"
-		break
-
-	elif [[ "$userInput" == "1" ]]; then
-		displayCoursesofInst
-
-	elif [[ "$userInput" == "2" ]]; then
-		courseCountofInsts
-
-	# TODO - 3 Display a message, if an invalid input is given
-	fi
+    case $userInput in
+        1) displayCoursesofInst ;;
+        2) courseCountofInsts ;;
+        3) classlocations ;;
+        4) openseats ;;
+        5) echo "Goodbye" && exit ;;
+        *) echo "Invalid input. Please try again." ;;
+    esac
 done
